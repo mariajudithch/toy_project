b4ac5c674696b938a0f7c40a3c3db01535e948e4
Maria Judith Carmona Higuita <juditycarmona@gmail.com>
2026-09-17 12:02:46 +0200

    push

--- analysis.R
+++ analysis.R
@@ -1,2 +1,5 @@
 library(usethis)
-usethis::use_git()
+usethis::use_git() # to create a repo
+usethis::git_default_branch("main") # to change the branch
+usethis::use_github() # to contect it to the cloud
+usethis::use_git(message = "gitignore was modified") # to make commits
